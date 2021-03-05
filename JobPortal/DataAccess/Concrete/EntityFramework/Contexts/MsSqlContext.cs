392d02f80ac886c321aebac8a13ab18975e58eff
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2021-03-05 22:16:26 +0300

    Dal interfaces were created in the DataAccess layer, and added dal classes especially for EntityFramework.

---
+++ JobPortal/DataAccess/Concrete/EntityFramework/Contexts/MsSqlContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public partial class MsSqlContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MsSqlLocalDb;Database=JobPortal;Trusted_Connection=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Advertisement> Advertisements { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<EfCompanyDal> Companies { get; set; }
+        public DbSet<CurriculumVitae> CurriculumVitaes { get; set; }
+    }
+}
