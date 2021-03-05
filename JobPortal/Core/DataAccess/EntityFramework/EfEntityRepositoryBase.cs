a88219f5d69abdd3af8a0572149be54d82d4a297
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2021-03-05 21:32:41 +0300

    IEntityRepository and EfEntityRepositoryBase structure was included to the project.

---
+++ JobPortal/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            TContext context = new TContext();
+            if (filter == null)
+            {
+                return await context.Set<TEntity>().ToListAsync();
+            }
+            else
+            {
+                return await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().FirstOrDefault(filter);
+            }
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            TContext context = new TContext();
+            return await context.Set<TEntity>().FirstOrDefaultAsync(filter);
+        }
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            TContext context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            await context.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            TContext context = new TContext();
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            TContext context = new TContext();
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ JobPortal/Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T: class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        Task DeleteAsync(T entity);
+        void Update(T entity);
+        Task UpdateAsync(T entity);
+    }
+}
